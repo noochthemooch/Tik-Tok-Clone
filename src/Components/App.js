419bbdffc9ef830330e54890cf0808f5e68bc280
noochthemooch <31193558+noochthemooch@users.noreply.github.com>
2023-04-28 19:54:14 -0500

    Update App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -33,7 +33,7 @@ function App() {
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(
-        "https://www.reddit.com/r/NSFWFunny/.json?limit=100"
+         "https://www.reddit.com/r/tiktokcringe/.json?limit=100"
       );
 
       const data = await response.json();
