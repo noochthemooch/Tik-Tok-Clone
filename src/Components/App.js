c4014a9d1d3d54e3afe9bd5d523004dd0ae46a0d
noochthemooch <31193558+noochthemooch@users.noreply.github.com>
2023-04-28 21:11:33 -0500

    Update App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -33,7 +33,7 @@ function App() {
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(
-         "https://www.reddit.com/r/CrazyFuckingVideos/.json?limit=100"
+         "https://www.reddit.com/r/NSFWfunny/.json?limit=100"
       );
 
       const data = await response.json();
